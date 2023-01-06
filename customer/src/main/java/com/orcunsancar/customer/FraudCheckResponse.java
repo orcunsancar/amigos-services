4c070ae1ecf25a5e39fc4c9ce8e643c71d2cc931
orcunsancar <eorcunsancar@gmail.com>
2023-01-06 23:11:46 +0300

    add fraud service and restTemplate

---
+++ customer/src/main/java/com/orcunsancar/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.orcunsancar.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/com/orcunsancar/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.orcunsancar.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
