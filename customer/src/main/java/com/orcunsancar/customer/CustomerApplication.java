a1177ba593499b531d8130f2f7fba0005dee31b9
orcunsancar <eorcunsancar@gmail.com>
2023-01-17 21:52:50 +0300

    rabbitmq added for producing by customer and consumed by notification

--- customer/src/main/java/com/orcunsancar/customer/CustomerApplication.java
+++ customer/src/main/java/com/orcunsancar/customer/CustomerApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.orcunsancar.customer",
+                "com.orcunsancar.amqp"
+        }
+
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.orcunsancar.clients"
--- customer/src/main/java/com/orcunsancar/customer/CustomerService.java
+++ customer/src/main/java/com/orcunsancar/customer/CustomerService.java
@@ -1,8 +1,8 @@
 package com.orcunsancar.customer;
 
+import com.orcunsancar.amqp.RabbitMQMessageProducer;
 import com.orcunsancar.clients.fraud.FraudCheckResponse;
 import com.orcunsancar.clients.fraud.FraudClient;
-import com.orcunsancar.clients.notification.NotificationClient;
 import com.orcunsancar.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +13,7 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
-    private final NotificationClient notificationClient;
+    private final RabbitMQMessageProducer producer;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -31,14 +31,17 @@ public class CustomerService {
         if(fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
-        // todo: make it async. i.e add to queue
-        notificationClient.sendNotification(
-                new NotificationRequest(
-                        customer.getId(),
-                        customer.getEmail(),
-                        String.format("Hi %s, welcome to Amigoscode...",
-                                customer.getFirstName())
-                )
+
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome to Amigoscode...",
+                        customer.getFirstName())
+        );
+        producer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
         );
     }
 }
